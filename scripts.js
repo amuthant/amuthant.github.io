a0f905a942f02a0ce396a6fc98b51e06566d530d
amuthant <amuthanthennavan@gmail>
2024-09-14 01:07:31 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.nav__links').classList.toggle('active');
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
